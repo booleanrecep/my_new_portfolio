c705fff0b9b3a0171601fa9a71016c6120e0de1c
Recep Öztürk <recep.fed@gmail.com>
2021-03-23 23:11:33 +0300

    refactored the code and cleaned the file structered

--- src/App.js
+++ src/App.js
@@ -1,52 +1,26 @@
-import React, { Component } from 'react';
-import $ from 'jquery';
+import React from 'react';
+
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
 import Portfolio from './Components/Portfolio';
 
-class App extends Component {
+import { data } from "./data"
+const { main, resume, portfolio } = data;
 
-  constructor(props){
-    super(props);
-    this.state = {
-      foo: 'bar',
-      resumeData: {}
-    };
-  }
+const App = () => {
 
-  getResumeData(){
-    $.ajax({
-      url:'/resumeData.json',
-      dataType:'json',
-      cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
-
-  componentDidMount(){
-    this.getResumeData();
-  }
-
-  render() {
-    return (
-      <div className="App">
-       
-        <Header data={this.state.resumeData.main}/>
-        <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>
-        <Footer data={this.state.resumeData.main}/>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Header {...main} />
+      <About {...main} />
+      <Resume {...resume} />
+      <Portfolio {...portfolio} />
+      <Footer {...main} />
+    </div>
+  )
 }
+
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,150 @@
+export const data={
+    "main": {
+      "name":"Recep Öztürk",
+      "occupation":"Frontend Developer",
+      "description":"I blieve in hardworking, dedication and endeavor.",
+      "image":"recep.png",
+      "bio":"I am a ambitious front end develeoper. I am always on the way of learning new tecnologies.  I dedicate myself to learn a new things , to grasp a new aspect. I like to dive into prototype chain and understand what is happening behind the scene. Recently I am learning German.",
+      "contactmessage":null,
+      "email": "recep.fed@gmail.com",
+      "phone": "+90 553 209 3580",
+      "address":{
+        "street":"Maltepe",
+        "city":"Istanbul",
+        "state":"Turkey",
+        "zip": null
+      },
+      "website": "https://booleanrecep.github.io/",
+      "resume_link":"https://docs.google.com/document/d/1RNRmWLAtYKzRgpnN8BfzxbKVFI9grTSvSkYyID25lmI/edit?usp=sharing",
+      "social":[
+        {
+          "name":"facebook",
+          "url":"https://www.facebook.com/recepteachion/",
+          "className":"fa fa-facebook"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/booleanrecep",
+          "className":"fa fa-twitter"
+        },
+     
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/booleanrecep/",
+          "className":"fa fa-linkedin"
+        },
+        
+        {
+          "name":"github",
+          "url":"http://github.com/booleanrecep",
+          "className":"fa fa-github"
+        }
+        
+      ]
+    },
+    "resume":{
+      "skillmessage":null,
+      "education":[
+        {
+          "school":"Fırat University?",
+          "degree":"Turkish Literature",
+          "graduated":"June 2011",
+          "description":null
+        },
+        {
+          "school":"Re:Coded NGO",
+          "degree":"Frontend Development With React.js",
+          "graduated":"May 2020",
+          "description":null
+        }
+      ],
+      "work":[
+        {
+          "company":"National Education Ministry",
+          "title":"Literature Teacher ",
+          "years":"September 2011 - Present",
+          "description":null
+        },
+        {
+          "company":"Re:Coded",
+          "title":"Junior Frontend Developer",
+          "years":"January 2020- May 2010",
+          "description":null
+        }
+      ],
+      "skills":[
+        {
+          "name":"Git",
+          "level":"80%"
+        },
+        {
+          "name":"GitHub",
+          "level":"80%"
+        },
+        {
+          "name":"GitLab",
+          "level":"80%"
+        },      
+       
+        {
+          "name":"CSS",
+          "level":"70%"
+        },
+        {
+          "name":"HTML",
+          "level":"80%"
+        },
+        {
+          "name":"JavaScript",
+          "level":"70%"
+        },
+        {
+          "name":"ReactJs",
+          "level":"70%"
+        },
+        {
+          "name":"MongoDB",
+          "level":"50%"
+        },
+        {
+          "name":"GraphQL",
+          "level":"50%"
+        },
+        {
+          "name":"Three.js",
+          "level":"30%"
+        }
+      ]
+    },
+    "portfolio":{
+      "projects": [
+        
+        {
+          "title":"Landing Page of a Social Platform",
+          "category":null,
+          "image":"clive.png",
+          "url":"https://www.comitium.live/"
+        },
+        {
+          "title":"A Fake Full Stack Keep with MERN",
+          "category":null,
+          "image":"fake_keep.png",
+          "url":"pure-ravine-98958.herokuapp.com/"
+        },
+        {
+          "title":"A CRUD App for Homework",
+          "category":null,
+          "image":"odevne.png",
+          "url":"https://odevne.netlify.app/#/"
+        } ,
+        {
+          "title":"A Webinar Promotional Site",
+          "category":null,
+          "image":"webinar.png",
+          "url":"https://webinarapp.netlify.app/#/"
+        }          
+       
+      ]
+    }
+  }
+  
